47b6b68fb7e55298779d38a6bb20300d78621184
Eyup Atis <eyupatis@gmail.com>
2018-03-17 14:31:43 +0300

    make koudoku controllers extendable with decorators

--- lib/koudoku/engine.rb
+++ lib/koudoku/engine.rb
@@ -16,5 +16,10 @@ module Koudoku
       end
     end
 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
